afa2b85e6c27ad76823b661708b9893ca6e43f5a
João Messias Lima Pereira <jmlp@ic.ufal.br>
2020-04-09 18:34:22 -0300

    ...

--- src/main/java/plus/json_models/CodeModel.java
+++ src/main/java/plus/json_models/CodeModel.java
@@ -4,11 +4,6 @@ public class CodeModel {
     public String result;
     public String code;
 
-    public CodeModel(String result, String code) {
-        this.result = result;
-        this.code = code;
-    }
-
     @Override
     public String toString() {
         return "CodeModel{" +
